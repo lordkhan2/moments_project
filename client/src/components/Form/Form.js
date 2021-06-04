2baa7b8c2af4162e1becc9e36bad8b602bf4b33f
lordkhan2 <70976558+lordkhan2@users.noreply.github.com>
2021-06-05 01:46:38 +0600

    Update Form.js

--- client/src/components/Form/Form.js
+++ client/src/components/Form/Form.js
@@ -3,49 +3,48 @@ import React, {useState, useEffect } from 'react';
 import { TextField, Button, Typography, Paper } from '@material-ui/core';
 import useStyles from './styles';
 import FileBase from 'react-file-base64';
-//to call dispatch and useSelector to view
+
 import { useDispatch, useSelector } from 'react-redux';
 
-//import createPost
+
 import {createPost, updatePost} from '../../actions/posts';
 
 
 
-//set currentId and setCurrentID as props
+
 const Form = ({ currentId, setCurrentId }) => {
     const classes =useStyles();
     const [postData, setPostData] = useState({creator: '', title: '', message: '', tags: '', selectedFile: ''});
-    //if we have currentid then find post that has same id as our current if, if not return null
+    
     const post = useSelector((state) => (currentId ? state.posts.find((message) => message._id === currentId) : null));
-    // use dispatch function
+    
     const dispatch = useDispatch();
 
-    //useEffect to populated values in the form
-    //
+    
     useEffect(() => {
-        //if  post exsists we set post data
+        
         if (post) setPostData(post);
     }, [post]);
-    //[] dependency array// which is used to say when a callback function should be ran// for this case we say when post changes callback function is ran
+    
 
 
     const handleSubmit = (e) =>{
-        //dispatch on handle submit
+        
 
-        //to prevent page refresh after handle submit
+        
         e.preventDefault();
 
         if (currentId === null) {
             dispatch(createPost(postData));
             
           } else {
-              //update the post using patch
+              
             dispatch(updatePost(currentId, postData));
             
           }
         clear();
         setTimeout(() =>{
-            window.location.reload(); // you can pass true to reload function to ignore the client cache and reload from the server
+            window.location.reload(); 
         },1000);
     };
     const clear = () =>{
