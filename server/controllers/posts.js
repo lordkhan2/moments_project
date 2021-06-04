53a5ba8ffd8d12ffe31e00a7ebd39526f8630a3d
lordkhan2 <70976558+lordkhan2@users.noreply.github.com>
2021-06-05 01:52:06 +0600

    Update posts.js

--- server/controllers/posts.js
+++ server/controllers/posts.js
@@ -15,9 +15,7 @@ export const getPosts = async (req, res) => {
 }
 
 export const createPost = async (req, res) => {
-    //to get the body
     const post =req.body;
-    //creates new post
     const newPost = new PostMessage(post);
     try {
         await newPost.save();
@@ -31,18 +29,13 @@ export const createPost = async (req, res) => {
 }
 
 export const updatePost = async (req, res) => {
-    //extract id from req.params
-    //req.params is used to extract object, i.e id
-    //id is renamed to _id for mongoose
+    
     const {id: _id} =req.params;
-    //post is set to request body data
     const post = req.body;
-    //check if id is mongoose id
+ 
     if(!mongoose.Types.ObjectId.isValid(_id)) return res.status(404).send('No post with that id');
-    // call model, PostMessage, findByIdAndUpdate method is called which parameters are _id and post, 
-    //and new:true to receive updated version of post
     const updatePost = await PostMessage.findByIdAndUpdate(_id, {...post, _id}, { new: true});
-    //send over that updatePost with res,json
+   
     res.json(updatePost);
 }
 
@@ -60,9 +53,8 @@ export const deletePost = async (req, res) =>{
 export const likePost = async (req, res) => {
     const {id} = req.params;
     if (!mongoose.Types.ObjectId.isValid(id)) return res.status(404).send(`No post with id: ${id}`);
-    //to find the post we are looking for// returns post
+    
     const post = await PostMessage.findById(id);
-    //returns updatedPost
     const updatedPost = await PostMessage.findByIdAndUpdate(id, { likeCount: post.likeCount + 1 }, { new: true });
 
     res.json(updatedPost);
