ed03091e2ee36d854de9be5ceaf4ff63e0df3ac4
lordkhan2 <70976558+lordkhan2@users.noreply.github.com>
2021-06-05 01:52:50 +0600

    Update posts.js

--- server/routes/posts.js
+++ server/routes/posts.js
@@ -1,9 +1,5 @@
 import express from 'express';
-
-//not default export so {}
 import { getPosts, createPost, updatePost, deletePost, likePost } from '../controllers/posts.js'
-
-//routes folder does not contain logic
 
 const router = express.Router();
 
@@ -11,11 +7,8 @@ const router = express.Router();
 
 router.get('/', getPosts);
 router.post('/', createPost);
-//patch used to update post
 router.patch('/:id', updatePost);
-
 router.delete('/:id', deletePost);
-//liking something is updating
 router.patch('/:id/likePost', likePost);
 
 
